19010e4388752fb1fd6483346afdd5786e45be31
Zsolt Tóth <waysofdarkness@gmail.com>
2020-10-28 18:40:48 +0100

    Added fallback typedef.

--- SIMD/MhSIMD.hpp
+++ SIMD/MhSIMD.hpp
@@ -1,6 +1,6 @@
 #ifndef MHSIMD_HPP
 #define MHSIMD_HPP
-
+#include "private/fallback/MhFallbackVector.hpp"
 #ifdef __SSE4_1__
 #include "private/x86/MhFloat32x4_private.hpp"
 #include "private/x86/MhInt32x4_private.hpp"
@@ -21,15 +21,23 @@ namespace SIMD {
 
 #ifdef MHFLOAT32X4
 typedef Float32x4_private Float32x4;
+#else
+typedef FallbackVector<float,4> Float32x4;
 #endif
 #ifdef MHFLOAT32X8
 typedef Float32x8_private Float32x8;
+#else
+typedef FallbackVector<float,8> Float32x8;
 #endif
 #ifdef MHFLOAT64X2
 typedef Float64x2_private Float64x2;
+#else
+typedef FallbackVector<double,2> Float64x2;
 #endif
 #ifdef MHINT32X4
 typedef Int32x4_private Int32x4;
+#else
+typedef FallbackVector<int32_t,4> Int32x4;
 #endif
 
 }
